dafe7eb268487689141442cbddf26d8b4aa807a0
Rebecca Turner <637275@gmail.com>
2018-05-15 22:33:21 -0400

    the url editor works! it could use some styling but it's pretty OK!

--- content/url-editor.md
+++ content/url-editor.md
@@ -3,16 +3,26 @@ title="url editor"
 description="a live, interactive tool for editing urls and their parameters"
 js=["url-editor"]
 css=["url-editor", "unicode/tools"]
-draft=true
 
 [blackfriday]
 extensionsmask=["autolink"]
 +++
 
-a live, interactive tool for editing urls and their parameters
+a live, interactive tool for editing urls and their parameters. working beta
 
+url input:
 <textarea id="url_in">
 https://www.amazon.com/Cutter-Stripper-Stranded-Klein-Tools/dp/B00080DPNQ/ref=sr_1_6?s=power-hand-tools&ie=UTF8&qid=1526426148&sr=1-6&keywords=wire+strippers
 </textarea>
 
 <div id="url_out"></div>
+
+<noscript>nothing will happen without javascript</noscript>
+
+# help
+
+* you can edit all fields in boxes and your changes will update in the url
+  input box
+* you can click the × to delete a parameter or element of the path from the url
+* it needs a browser from about 2016 or later to work because of some
+  javascript stuff
--- static/js/url-editor.js
+++ static/js/url-editor.js
@@ -2,22 +2,33 @@
 //
 // URLSearchParams: https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
 
-// let me have ONE global ok
+// a URL with an added .pathList, a list of strings with one per path-segment;
+// corresponds to .pathname. when you edit .pathList, call .updatePath as well
+// to keep everything in sync
 var url
 
 let $ = id => document.getElementById(id)
 
+let input_change_fn = fn => {
+	return e => {
+		fn(e)
+		display_url()
+	}
+}
+
 // opts:
 // edit: bool (is editable)
-// onchange: fn (callback for edit)
+// change: fn (callback for edit)
 // class: str (element class)
 function make_field(value, opts={}) {
 	var key
 	if(opts.edit) {
 		key = document.createElement('input')
 		key.value = value
-		if(opts.onchange) {
-			key.addEventListener('change', opts.onchange, false)
+		if(opts.change) {
+			let handler = input_change_fn(opts.change)
+			key.addEventListener('change', handler, false)
+			key.addEventListener('input', handler, false)
 		}
 	} else {
 		key = document.createElement('span')
@@ -27,6 +38,20 @@ function make_field(value, opts={}) {
 		key.className = opts.class
 	}
 	return key
+}
+
+let delete_func = fn => {
+	return e => {
+		fn(e.target.parentElement)
+		display_url()
+	}
+}
+
+function make_delete_link(fn, txt='×') {
+	let delete_link = document.createElement('a')
+	delete_link.addEventListener('click', fn)
+	delete_link.innerHTML = txt
+	return delete_link
 }
 
 // accepted keys in opts:
@@ -64,7 +89,10 @@ function make_row(key, val, opts={}) {
 		}
 		new_row.append(make_field(val, opts.val))
 	}
-	// TODO deletion code
+	if(opts.delete) {
+		// TODO icon?
+		new_row.append(make_delete_link(delete_func(opts.delete)))
+	}
 	// TODO reordering code
 	return new_row
 }
@@ -93,10 +121,10 @@ let set_path = newpath => '/' + newpath
 // and non-empty
 //
 // opts: an add_row opts dict with the following keys
-// val_fn is applied to val if val is defined and non-empty
+// val: fn: applied to val if val is defined and non-empty
 function maybe_static_row(key, val, opts={}) {
 	if(val !== undefined && val !== "") {
-		if(opts.val_fn) {
+		if(opts.val && opts.val.fn) {
 			val = opts.val_fn(val)
 		}
 		if(opts.key === undefined) {
@@ -117,21 +145,31 @@ function maybe_mutable_row(key, val, opts={}) {
 		if(opts.val_fn) {
 			val = opts.val_fn(val)
 		}
+
 		if(opts.key === undefined) {
 			opts.key = {}
 		}
-		opts.key.edit = true
+		if(opts.key.edit === undefined) {
+			opts.key.edit = true
+		}
+
 		if(opts.val === undefined) {
 			opts.val = {}
 		}
-		opts.val.edit = true
+		if(opts.val.edit === undefined) {
+			opts.val.edit = true
+		}
+
 		add_row(key, val, opts)
 	}
 }
 
+let get_section = sec => document.getElementById('section-' + sec)
+
 function make_section(title) {
 	let ret = document.createElement('div')
-	ret.className = 'section section-' + title.replace(/ /g, '-')
+	ret.className = 'section'
+	ret.setAttribute('id', 'section-' + title.replace(/ /g, '-'))
 	if(title) {
 		let header = document.createElement('h2')
 		header.innerHTML = title
@@ -140,61 +178,136 @@ function make_section(title) {
 	return ret
 }
 
+let get_key = row => row.getElementsByClassName('key')[0]
+let get_val = row => row.getElementsByClassName('val')[0]
+
+let delete_param = row => {
+	url.searchParams.delete(get_key(row).value)
+	row.remove()
+	if(url.search.length <= 1) {
+		// delete the whole section
+		get_section('parameters').remove()
+	}
+}
+
+let delete_all_params = e => {
+	url.search = ''
+	get_section('parameters').remove()
+	display_url()
+}
+
+// sets up the params portion of the page
 function parse_params(searchParams) {
 	let container = make_section('parameters')
+	let title = container.firstElementChild
+	title.append(make_delete_link(delete_all_params))
+
 	for(param of searchParams) {
 		add_row(param[0], param[1], {
+			delete: delete_param,
 			target: container,
 			key: { edit: true },
-			val: { edit: true },
+			val: { edit: true }
 		})
 	}
 	url_out.append(container)
 }
 
-function ul(items, func) {
-	let add_item = it => {
-		let li = document.createElement('li')
-		li.append(it)
-		list.append(li)
+let get_index = row => +row.getAttribute('data-index')
+let set_index = (row, inx) => row.setAttribute('data-index', inx)
+
+let decrement_path_indicies_after = i => {
+	let rows = get_section('path').getElementsByClassName('row')
+	var inx = -1
+	for(row of rows) {
+		inx = get_index(row)
+		if(inx > i) {
+			set_index(row, inx - 1)
+		}
 	}
-	for(item of items) {
-		add_item(func(item))
-	}
-	return list
 }
 
+let delete_path = row => {
+	inx = get_index(row)
+	url.pathList.splice(inx, 1)
+	url.updatePath()
+	decrement_path_indicies_after(inx)
+	row.parentElement.remove()
+}
+
+let change_path = e => {
+	row = e.target.parentElement
+	url.pathList[get_index(row)] = e.target.value
+	url.updatePath()
+}
+
+// sets up the path portion of the page
 function parse_path(pathname) {
 	let container = make_section('path')
 	let dirs = display_path(pathname).split('/')
 	let list = document.createElement('ul')
+	var i = 0
 	for(dir of dirs) {
 		let li = document.createElement('li')
 		li.append(make_row(undefined, dir, {
-			val: { edit: true }
+			delete: delete_path,
+			val: {
+				edit: true,
+				change: change_path,
+			}
 		}))
+		// first child is the div.row
+		li.firstElementChild.setAttribute('data-index', i)
 		list.append(li)
+		i++
 	}
 	container.append(list)
 	url_out.append(container)
 }
 
-function parse(url) {
-	url = new URL(url)
+// given a url, sets up everything
+function parse(newurl) {
+	url = new URL(newurl)
+	url.pathList = display_path(url.pathname).split('/')
+	url.updatePath = url_update_path
 	url_out.innerHTML = ''
-	maybe_mutable_row('protocol', url.protocol, { val_fn: display_protocol })
+	maybe_mutable_row('protocol', url.protocol, {
+		key: { edit: false },
+		val: {
+			fn: display_protocol,
+			change: e => url.protocol = set_protocol(e.target.value)
+		}
+	})
 	// hostname: composed of host:port
 	// origin: scheme, domain, and port
-	maybe_mutable_row('user', url.username)
-	maybe_mutable_row('password', url.password)
-	maybe_mutable_row('host', url.hostname)
-	maybe_mutable_row('port', url.port)
+	maybe_mutable_row('user', url.username, {
+		key: { edit: false },
+		val: { change: e => url.username = e.target.value }
+	})
+	maybe_mutable_row('password', url.password, {
+		key: { edit: false },
+		val: { change: e => url.password = e.target.value }
+	})
+	maybe_mutable_row('host', url.hostname, {
+		key: { edit: false },
+		val: { change: e => url.hostname = e.target.value }
+	})
+	maybe_mutable_row('port', url.port, {
+		key: { edit: false },
+		val: { change: e => url.port = e.target.value }
+	})
 	parse_path(url.pathname)
-	maybe_mutable_row('#', url.hash)
+	maybe_mutable_row('#', url.hash, {
+		key: { edit: false },
+		val: { change: e => url.hash = e.target.value }
+	})
 	parse_params(url.searchParams)
 }
 
 let parse_event = e => parse(e.target.value)
+let display_url = () => url_in.value = url.href
+let url_update_path = () => url.pathname = '/' + url.pathList.join('/')
+let set_url_part = (part, val) => url[part] = val
 
 function init() {
 	parse(url_in.value)
