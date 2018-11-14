6b1ff07502a3d46ec129d486a7daee574388f593
www-data <www-data@becca.ooo>
2018-11-14 12:56:09 -0500

    post-merge script

--- post-merge.sh
+++ post-merge.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # keep this in sync with dev.ps1
-rm -rf static/css/*
+rm -rf static/css
+mkdir static/css
 sass --unix-newlines --sourcemap=none -E UTF-8 --update assets/styles:static/css
 hugo --verbose
