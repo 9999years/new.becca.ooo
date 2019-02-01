665aa16b0b62a053b15a65256c1a78aaf4d2a136
Rebecca Turner <637275@gmail.com>
2019-02-01 11:59:27 -0500

    lfs checkout in post-merge script

--- post-merge.sh
+++ post-merge.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # keep this in sync with dev.ps1
+git lfs checkout
 rm -rf static/css
 mkdir static/css
 sass --unix-newlines --sourcemap=none -E UTF-8 --update assets/styles:static/css
