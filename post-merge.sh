b41739ee5414d7cd27d529fb2279a61cdd99f9b1
Rebecca Turner <637275@gmail.com>
2019-04-09 20:00:31 -0400

    Better Travis script
    - Updated Hugo from 0.38 -> 0.55
    - Updated Sass from ruby-sass to dart-sass (!)
    - Added yamllint step

--- post-merge.sh
+++ post-merge.sh
@@ -5,5 +5,5 @@ git lfs fetch
 git lfs checkout
 rm -rf static/css
 mkdir static/css
-sass --unix-newlines --sourcemap=none -E UTF-8 --update assets/styles:static/css
+sass assets/styles:static/css
 hugo --verbose
