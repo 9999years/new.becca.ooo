ec2a3432fcda18714a59de770038b4affeb3f21d
Rebecca Turner <rbt@sent.as>
2020-02-25 13:38:51 -0500

    shell hook

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
 { pkgs ? import <nixpkgs> { inherit system; }, system ? builtins.currentSystem
 }@args:
 
-(import ./override.nix args).shell.overrideAttrs
-(old: { buildInputs = old.buildInputs ++ (with pkgs; [ just ]); })
+(import ./override.nix args).shell.overrideAttrs (old: {
+  buildInputs = old.buildInputs ++ (with pkgs; [ just ]);
+  shellHook = ''
+    ${old.shellHook}
+    if [[ ! -e ]]
+    then
+      ln -s $nodeDependencies/lib/node_modules node_modules
+    fi
+  '';
+})
